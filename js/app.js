c668a0d3ffa634c23d77642e0350485d510d697e
Grant <feast5801@hotmail.co.uk>
2021-02-07 15:29:42 +0000

    Add css grid and content
    Added CSS Grid and content to the main page aswel as media queries and hover effects.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 let mainNav = document.querySelector('#js-menu');
 let navBarToggle = document.querySelector('#js-navbar-toggle');
 
+// When user clicks on the toggle icon, a class is added to display nav items.
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
