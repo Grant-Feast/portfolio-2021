414b4b3ce2a3eea920207f5920444827f06832c0
Grant <feast5801@hotmail.co.uk>
2021-03-17 14:29:23 +0000

    Add blur effect to image
    Add a blur effect with a percentage timer on top of the image which increases as the image loads.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,28 @@
 let mainNav = document.querySelector('#js-menu');
 let navBarToggle = document.querySelector('#js-navbar-toggle');
 
+const numberLoad = document.querySelector('.number-load');
+const introImg = document.querySelector('.introduction-image');
+
 // When user clicks on the toggle icon, a class is added to display nav items.
 navBarToggle.addEventListener('click', function () {
   mainNav.classList.toggle('active');
 });
+
+let load = 0;
+let int = setInterval(blurring, 40);
+
+function blurring() {
+  load++;
+  if(load > 99) {
+    clearInterval(int);
+  }
+  numberLoad.innerText = `${load}%`;
+  numberLoad.style.opacity = scale(load, 0, 100, 1, 0);
+  introImg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+
+
+const scale = (num, in_min, in_max, out_min, out_max) => {
+  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min
+}
